7c4b7e341df1ec9321d97ff426a90492594717c5
jjack-1 <qazwsx0088@gmail.com>
2025-05-15 13:49:06 +0900

    CORS 허ì용처리

--- src/main/java/shop/mtcoding/blog/_core/config/FilterConfig.java
+++ src/main/java/shop/mtcoding/blog/_core/config/FilterConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import shop.mtcoding.blog._core.filter.AuthorizationFilter;
+import shop.mtcoding.blog._core.filter.CorsFilter;
 import shop.mtcoding.blog._core.filter.LogFilter;
 import shop.mtcoding.blog.user.UserRepository;
 
@@ -15,9 +16,9 @@ public class FilterConfig {
     private final UserRepository userRepository;
 
     @Bean
-    public FilterRegistrationBean<LogFilter> loggingFilter() {
-        FilterRegistrationBean<LogFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new LogFilter(userRepository));
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
         registrationBean.addUrlPatterns("/*"); // 모든 요청에 적용
         registrationBean.setOrder(1); // 필터 순서 설정
         return registrationBean;
@@ -29,6 +30,15 @@ public class FilterConfig {
         registrationBean.setFilter(new AuthorizationFilter());
         registrationBean.addUrlPatterns("/s/*"); // 모든 요청에 적용
         registrationBean.setOrder(2); // 필터 순서 설정
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> loggingFilter() {
+        FilterRegistrationBean<LogFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LogFilter(userRepository));
+        registrationBean.addUrlPatterns("/*"); // 모든 요청에 적용
+        registrationBean.setOrder(3); // 필터 순서 설정
         return registrationBean;
     }
 }
---
+++ src/main/java/shop/mtcoding/blog/_core/filter/CorsFilter.java
@@ -0,0 +1,38 @@
+package shop.mtcoding.blog._core.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String origin = request.getHeader("Origin");
+        log.debug("origin : " + origin);
+
+        response.setHeader("Access-Control-Allow-Origin", "");
+//        response.setHeader("Access-Control-Expose-Headers", "Authorization"); // 이 헤더 응답을 자바스크립트로 접근 할 수 있도록 허용할지
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Key, Content-Type, Accept, Authorization"); // 서버가 요청을 받을 때 이 헤더값은 허용해서 받는다
+        // X-key -> 내가 임의로 만든 헤더 값은 X- 를 붙여서 만든다. 약속이다
+        response.setHeader("Access-Control-Allow-Credentials", "true"); // 쿠키의 세션값을 허용
+
+        // 위의 응답 헤더값을 브라우저가 읽어서
+
+        // Preflight 요청을 허용하고 바로 응답하는 코드
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(200);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
